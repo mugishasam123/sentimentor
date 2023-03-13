f22b9cdca71112b2dfae4c6ea9bcff2215814cdc
mugishasam123 <mugishasamuel42@gmail.com>
2023-03-13 14:10:38 +0200

    Add sentiment controller

---
+++ controllers/sentiment.controller.js
@@ -0,0 +1,44 @@
+const aposToLexForm = require("apos-to-lex-form");
+const natural = require("natural");
+const SpellCorrector = require("spelling-corrector");
+const SW = require("stopword");
+
+const spellCorrector = new SpellCorrector();
+spellCorrector.loadDictionary();
+
+const analyseHandler = (req, res) => {
+  const { review } = req.body;
+  /* NORMALIZATION */
+
+  // negation handling
+  // convert apostrophe=connecting words to lex form
+  const lexedReview = aposToLexForm(review);
+
+  // casing
+  const casedReview = lexedReview.toLowerCase();
+
+  // removing
+  const alphaOnlyReview = casedReview.replace(/[^a-zA-Z\s]+/g, "");
+
+  // tokenize review
+  const { WordTokenizer } = natural;
+  const tokenizer = new WordTokenizer();
+  const tokenizedReview = tokenizer.tokenize(alphaOnlyReview);
+
+  // spell correction
+  tokenizedReview.forEach((word, index) => {
+    tokenizedReview[index] = spellCorrector.correct(word);
+  });
+
+  // remove stopwords
+  const filteredReview = SW.removeStopwords(tokenizedReview);
+
+  const { SentimentAnalyzer, PorterStemmer } = natural;
+  const analyzer = new SentimentAnalyzer("English", PorterStemmer, "afinn");
+
+  const analysis = analyzer.getSentiment(filteredReview);
+
+  res.status(200).json({ analysis });
+};
+
+module.exports = analyseHandler;
